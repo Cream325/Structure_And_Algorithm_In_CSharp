3ddf2fa7f1be9b1f1a2c56e9ed1c3481843c4f5a
Cream325 <pto5515@gmail.com>
2024-05-20 23:33:54 +0900

    1. TreeNode 추가 2. AbstractLCRSTree 추가 및 구현

---
+++ Structure_And_Algorithm/Structure/No0_Nodes/TreeNode.cs
@@ -0,0 +1,24 @@
+﻿using Structure_And_Algorithm.Structure.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure_And_Algorithm.Structure.Nodes
+{
+    public class TreeNode : Node
+    {
+        #region Member Fields
+        private TreeNode childNode;
+        private TreeNode siblingNode;
+        #endregion
+
+        #region Properties
+        public TreeNode ChildNode { get => childNode; set => childNode = value; }
+        public TreeNode SiblingNode { get => siblingNode; set => siblingNode = value; }
+        #endregion
+
+        public TreeNode(object data) : base(data) { }
+    }
+}
---
+++ Structure_And_Algorithm/Structure/No5_Tree/AbstractLCRSTree.cs
@@ -0,0 +1,32 @@
+﻿using Structure_And_Algorithm.Structure.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure_And_Algorithm.Structure.Tree
+{
+    public abstract class AbstractLCRSTree
+    {
+        private TreeNode rootNode;
+
+        public TreeNode RootNode { get => rootNode; set => rootNode = value; }
+
+        #region Abstract Methods
+
+        #region Insert
+        public abstract void Insert(TreeNode parentNode, TreeNode newNode);
+        #endregion
+
+        #region Delete
+        public abstract void Delete();
+        #endregion
+
+        #region Traversal
+        public abstract void Traversal();
+        #endregion
+
+        #endregion
+    }
+}
